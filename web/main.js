52ef1cc440709f26a21cda3cfb05e47013201f1f
Drowrin <drowrin@gmail.com>
2024-05-16 14:23:11 -0400

    remove js redundancy

--- web/main.js
+++ web/main.js
@@ -1,1 +1,32 @@
-import "./entrypoint.js";
+"use strict";
+
+import "htmx.org";
+import "./fix_htmx_ext";
+import "htmx.org/dist/ext/preload";
+import "htmx.org/dist/ext/head-support";
+import "prismjs";
+import "prismjs/plugins/autoloader/prism-autoloader";
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.dataset.theme = "dark";
+} else {
+  document.documentElement.dataset.theme = "light";
+}
+
+window.toggle_dark_mode = function toggle_dark_mode() {
+  let t = document.documentElement.dataset.theme === "dark" ? "light" : "dark";
+  localStorage.theme = t;
+  if (t === "dark") {
+    document.documentElement.dataset.theme = "dark";
+  } else {
+    document.documentElement.dataset.theme = "light";
+  }
+};
+
+window.remove_empty = function remove_empty(obj) {
+  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v != ""));
+};
