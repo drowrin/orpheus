55ca7e181c944b71852c3f25b46be518dc17afd1
drowrin <drowrin@gmail.com>
2025-09-17 21:14:47 -0400

    use default today's date for files not in git

--- src/plugins/gitdates.mjs
+++ src/plugins/gitdates.mjs
@@ -6,23 +6,37 @@ export default function () {
     if (file.history[0].includes('/posts/')) {
       const filepath = file.history[0]
 
-      const published = file.data.astro.frontmatter.published
-        || execSync(
-          `git log --follow --diff-filter=A --format="%as" -1 -- "${filepath}"`,
-        ).toString().trim()
+      let published
+      try {
+        published = file.data.astro.frontmatter.published
+          || execSync(
+            `git log --follow --diff-filter=A --format="%as" -1 -- "${filepath}"`,
+          ).toString().trim()
+      }
+      catch {
+        published = new Date().toISOString().split('T')[0]
+      }
 
       const content = fs.readFileSync(filepath, 'utf8')
 
       const fmLines = 2 + content.split('---')[1].split('\n').length
       const totalLines = content.split('\n').length
 
-      const revisions = execSync(
-        `git log --diff-filter=M --format="%as: %s" --no-patch "-L${fmLines},${totalLines}:${filepath}"`,
-      ).toString().trim()
+      let revisions
+      let updated
+      try {
+        revisions = execSync(
+          `git log --diff-filter=M --format="%as: %s" --no-patch "-L${fmLines},${totalLines}:${filepath}"`,
+        ).toString().trim()
+        updated = revisions.split('\n')[0].split(':')[0]
+      }
+      catch {
+        updated = published
+      }
 
       file.data.astro.frontmatter.published = published
       file.data.astro.frontmatter.revisions = revisions
-      file.data.astro.frontmatter.updated = revisions.split('\n')[0].split(':')[0]
+      file.data.astro.frontmatter.updated = updated
     }
   }
 }
