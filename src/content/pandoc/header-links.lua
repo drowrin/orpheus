8664d64fe140c882f2b986d8c0319513851b6ba9
drowrin <drowrin@gmail.com>
2025-10-03 20:46:56 -0400

    remove old pandoc filters

--- src/content/pandoc/header-links.lua
+++
@@ -1,9 +0,0 @@
-function Header(elem)
-    attr = {
-        class = "header-link"
-    }
-    permalink = pandoc.Link("#", "#" .. elem.attr.identifier, nil, attr)
-    toplink = pandoc.Link("↑", "#title", nil, attr)
-    elem.content = elem.content .. {permalink, toplink}
-    return elem
-end
